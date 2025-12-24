cb8a6a66ffe36d53e3f4cbfe919304acc87b379c
chkp-eddiek <120951940+chkp-eddiek@users.noreply.github.com>
2025-12-24 18:36:47 +0200

    fix(VSECPC-12470): Adding support for security_groups in autoscale_gwlb module (#26)
    Co-authored-by: guybarak <guybarak@checkpoint.com>

--- modules/autoscale_gwlb/main.tf
+++ modules/autoscale_gwlb/main.tf
@@ -8,22 +8,50 @@ resource "aws_security_group" "permissive_sg" {
   name_prefix = format("%s_PermissiveSecurityGroup", local.asg_name)
   description = "Permissive security group"
   vpc_id = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = [for rule in var.security_rules : rule if rule.direction == "ingress"]
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+
+  dynamic ingress {
+    for_each = length([for rule in var.security_rules : rule if rule.direction == "ingress"]) == 0 ? [1] : []
+    content{
+        from_port    = 0
+        to_port      = 0
+        protocol     = "-1"
+        cidr_blocks  = ["0.0.0.0/0"]
+    }
+  }
+
+  dynamic "egress" {
+    for_each = [for rule in var.security_rules : rule if rule.direction == "egress"]
+    content {
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+    }
+  }
+
+  dynamic egress {
+    for_each = length([for rule in var.security_rules : rule if rule.direction == "egress"]) == 0 ? [1] : []
+    content{
+        from_port    = 0
+        to_port      = 0
+        protocol     = "-1"
+        cidr_blocks  = ["0.0.0.0/0"]
+    }
+  }
   tags = {
     Name = format("%s_PermissiveSecurityGroup", local.asg_name)
-    }
-  }  
-
-resource "aws_vpc_security_group_ingress_rule" "ingress_rule_ipv4" {
-  security_group_id = aws_security_group.permissive_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-    }
-
-resource "aws_vpc_security_group_egress_rule" "egress_rule_ipv4" {
-  security_group_id = aws_security_group.permissive_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
   }
+}
 
 resource "aws_vpc_security_group_ingress_rule" "ingress_rule_ipv6" {
   count = var.enable_ipv6 ? 1 : 0
